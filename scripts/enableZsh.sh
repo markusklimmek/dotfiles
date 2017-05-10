739668e6b32fb596c6747e993a53534b9fca5fab
Markus Klimmek <mklimmek@gmx.de>
2017-05-10 12:40:37 +0200

    Check if zsh is installed before changing the shell

--- scripts/enableZsh.sh
+++ scripts/enableZsh.sh
@@ -1,2 +1,11 @@
-echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/zsh
+# Is zsh installed? (Not idempotent at the moment)
+BREW_LIST=`brew list`
+if [[ "$BREW_LIST" == *"zsh"* ]] ; then
+  # It is not running at the moment
+  if [ -z "$ZSH_VERSION"]; then
+    # TODO: Check if /etc/shells already contains this line
+    echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
+    chsh -s /usr/local/bin/zshlog
+  fi
+fi
+
